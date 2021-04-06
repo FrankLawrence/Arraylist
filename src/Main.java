a8fb796ca95ce289dd079cd95aa95b3c3ad51e0d
Frank Lawrence <frankl.am.htg@gmail.com>
2021-04-06 16:30:30 +0200

    Changed Main class to work accordingly

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,9 @@ public class Main {
     public static void main(String[] args) {
         Bank bank1 = new Bank();
         Conto konto1 = new Conto(100);
-        bank1.kontoHinzufuegen(konto1);
         Kunde kunde1 = new Kunde("Lawrence", "Frank");
         kunde1.kontoHinzufuegen(konto1);
+        bank1.kundeHinzufuegen(kunde1);
         kunde1.anzeigen();
     }
 }
